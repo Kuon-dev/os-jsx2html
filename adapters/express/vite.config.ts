6a4688a9e01e1ed966f0a81e3c7a8e18f9632cf3
Kuon-dev <aaronlyn88@gmail.com>
2023-10-19 22:56:47 +0800

    feat: updated package json

--- adapters/express/vite.config.ts
+++ adapters/express/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { nodeServerAdapter } from "@builder.io/qwik-city/adapters/node-server/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
@@ -5,6 +6,7 @@ import baseConfig from "../../vite.config";
 export default extendConfig(baseConfig, () => {
   return {
     build: {
+      outDir: path.join(__dirname, "../../dist"),
       ssr: true,
       rollupOptions: {
         input: ["src/entry.express.tsx", "@qwik-city-plan"],
